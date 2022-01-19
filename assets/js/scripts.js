bf239f9642e5361dfdec8a341cd8d8162cf65cfa
TarikElnaggar <tarik.elnaggar@egabi.com>
2022-01-19 10:48:08 +0200

    update

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+//side nave
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+
